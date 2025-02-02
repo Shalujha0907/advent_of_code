df40750998f7dc4eb73cb5528f45c3a947abf2fb
Shalu Jha <183721575+Shalujha0907@users.noreply.github.com>
2025-02-02 20:15:11 +0530

    add part2 of day4

--- year_2019/day_4/part_1.js
+++ year_2019/day_4/part_1.js
@@ -1,29 +1,30 @@
-// 122345
-
-const areAdjacentValueEqual = (number) => {
-  const array = (number + "").split("");
-
-  return array.some((number, i) => {
-    if (i === array.length - 1) return false;
-    return number === array[i + 1];
-  });
+const areAdjacentValueEqual = (array) => {
+  for (let index = 0; index < array.length; index++) {
+    if (
+      array[index] !== array[index - 1] &&
+      array[index] === array[index + 1] &&
+      array[index + 1] !== array[index + 2]
+    ) {
+      return true;
+    }
+  }
+  return false;
 };
 
-const isAscendingOrder = (number) => {
-  const array = (number + "").split("");
+// console.log(areAdjacentValueEqual([1, 1, 1, 2]));
 
+const isAscendingOrder = (array) => {
   return array.every((number, i) => {
     if (i === array.length - 1) return true;
     return number <= array[i + 1];
   });
 };
 
-// console.log(isAscendingOrder(1233));
-
 const getValidPassword = (range1, range2) => {
   let count = 0;
   for (let index = range1; index < range2; index++) {
-    if (areAdjacentValueEqual(index) && isAscendingOrder(index)) {
+    const array = (index + "").split("");
+    if (areAdjacentValueEqual(array) && isAscendingOrder(array)) {
       count++;
     }
   }
